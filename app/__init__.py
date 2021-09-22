a4322d4d9ab05e6e02b4385582a3a0f98e0bf70d
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-22 20:55:28 +0300

    imported login manager then created an instance.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-def create_app():
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
     app = Flask(__name__)
     
     from .auth import auth as auth_blueprint
@@ -14,5 +19,6 @@ def create_app():
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     
     return app
