8a7510452962346334c5f4092052ca842e5a3bbc
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-22 23:20:36 +0300

    imported abort function then create a dynamic route.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,17 @@
 from flask import render_template, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
+from . import main
+from ..models import User
+
+@main.route('/user')
+@login_required
+def user():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return ('not found')
+    return render_template('profile.html', user = user)
+
 
 @main.route('/movie/review/new/<int:id>', methods=['GET', 'POST'])
 @login_required
