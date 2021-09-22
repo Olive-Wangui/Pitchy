9e1cf23132b2202721cd462a9826e52cfff8b434
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-22 23:04:14 +0300

    created an authenticated route logout.

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm
 from .. import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login')
@@ -33,3 +33,9 @@ def login():
         
         title = "watchlist login"
         return render_template('auth/login.html', login_form = login_form, title = title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
