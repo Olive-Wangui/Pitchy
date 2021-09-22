b7fd8e8196bbb992d89d0951ed9c571461c296cc
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-22 23:28:31 +0300

    created a new form class.

---
+++ app/main/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('bio', validators = [Required()])
+    submit = SubmitField('Post')
