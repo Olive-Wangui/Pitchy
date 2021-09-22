0b9e6c6dbca4d43bffebeb90c7e0bf5cff8fb70f
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-23 01:00:03 +0300

    set up some config files to connect to the gmail smtp server.

--- config.py
+++ config.py
@@ -6,4 +6,12 @@ engine = create_engine('postgresql+psycopg2://olive:behanna2@localhost/pitchy')
 class Config:
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://olive:behanna2@localhost/pitchy' 
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://olive:behanna2@localhost/pitchy'
+    
+      #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") 
