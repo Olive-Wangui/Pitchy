c289737aa2b530c6ae56a299597d17632459bdcc
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-22 21:42:29 +0300

    imported a few items and created a RegisterationForm form class.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from . import views, forms
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
