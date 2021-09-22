d5cd42f21258b202b469c3d7709056f5faf8ee93
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-22 20:46:40 +0300

    registered blueprint instance in the create_app function.

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,9 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-     
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
     
     # Initializing flask extensions
     bootstrap.init_app(app)
