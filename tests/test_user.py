979323992cd38284e90aed1aafec9ff3dba1c20e
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-23 12:14:58 +0300

    created a comment class, upvote and downvote class database.

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,10 @@
-import unittest
-from app.models import User
+# import unittest
 
-class UserModelTest(unittest.TestCase):
-    
-    def setUp(self):
-        self.new_user = User(password = 'ciara')
-        
-    def test_password_setter(self):
-        self.assertTrue(self.new_user.pass_secure is not None)
-        
-    def test_no_access_password(self):
-        with self.assertRaises(AttributeError):
-            self.new_user.password
-            
-    def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('ciara'))
+
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqual(True, False)
+
+
+# if __name__ == '__main__':
+#     unittest.main()
