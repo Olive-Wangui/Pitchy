d84778837800f3488e1ffc8ca8029da90f5d5f2d
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-23 13:21:21 +0300

    updated views.py.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
-from . import auth
-from ..models import User
-from .forms import RegistrationForm
-from .. import db
-from flask_login import login_user, logout_user, login_required
-from .forms import LoginForm, RegistrationForm
-from ..email import mail_message
+from app.auth import auth
+from app.models import User
+from flask_login import login_user, logout_user
+from ..email import email_message
 
 @auth.route('/login',  methods=['GET', 'POST'])
 def login():
@@ -26,20 +23,6 @@ def login():
         login_user(user)
         return redirect('/')
     return render_template('login.html')
-
-
-@auth.route('/login', methods =['GET', 'POST'])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user, login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password')
-        
-        title = "watchlist login"
-        return render_template('auth/login.html', login_form = login_form, title = title)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -77,7 +60,6 @@ def signup():
 
 
 @auth.route('/logout')
-@login_required
 def logout():
     logout_user()
-    return redirect(url_for("main.index"))
+    return redirect(url_for('auth.login'))
