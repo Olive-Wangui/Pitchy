19c7bee482952e20b37e83dec79e49d58a17a7ca
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-23 11:31:01 +0300

    created a comment, upvotes and downvotes section.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,24 @@
 from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
 from . import main
-from ..models import Post, User
-from .forms import PostForm, UpdateProfile
+from ..models import Post, Comment, User, Upvote, Downvote
+from .forms import PostForm, CommentForm, UpdateProfile
+
+@main.route('/')
+def index():
+    posts = Post.query.all()
+    product = Post.query.filter_by(category = 'product').all()
+    idea = Post.query.filter_by(category = 'idea').all()
+    business = Post.query.filter_by(category = 'Business').all()
+    return render_template('index.html', business = business, product = product, idea = idea, posts = posts)
+
+@main.route('/posts')
+@login_required
+def posts():
+    posts = Post.query.all()
+    likes = Upvote.query.all()
+    user = current_user
+    return render_template('pitch_display.html', posts = posts, likes = likes, user = user)
 
 @main.route('/new_post', methods=['GET', 'POST'])
 @login_required
@@ -18,6 +34,28 @@ def new_post():
         return redirect(url_for('main.index'))
     return render_template('pitch.html', form = form)
 
+@main.route('/comment/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def comment(post_id):
+    form = CommentForm()
+    post = Post.query.get(post_id)
+    user = User.query.all()
+    comments = Comment.query.filter_by(post_id=post_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        post_id = post_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(
+            comment=comment,
+            post_id=post_id,
+            user_id=user_id
+        )
+        new_comment.save()
+        new_comments = [new_comment]
+        print(new_comments)
+        return redirect(url_for('.comment', post_id=post_id))
+    return render_template('comment.html', form = form, post = post, comments = comments, user = user)
+
 @main.route('/user')
 @login_required
 def user():
@@ -31,12 +69,28 @@ def user():
 @login_required
 def updateprofile(name):
     form = UpdateProfile()
-    user = User.query.filter_by(username=name).first()
+    user = User.query.filter_by(username = name).first()
     if user is None:
         error = 'The user does not exist'
     if form.validate_on_submit():
         user.bio = form.bio.data
         user.save()
-        return redirect(url_for('.profile', name=name))
-    return render_template('profile/update_profile.html', form=form)
+        return redirect(url_for('.profile', name = name))
+    return render_template('profile/update_profile.html', form = form)
 
+@main.route('/like/<int:id>', methods=['POST', 'GET'])
+@login_required
+def upvote(id):
+    post = Post.query.get(id)
+    vote_mpya = Upvote(post = post, upvote = 1)
+    vote_mpya.save()
+    return redirect(url_for('main.posts'))
+
+
+@main.route('/dislike/<int:id>', methods=['GET', 'POST'])
+@login_required
+def downvote(id):
+    post = Post.query.get(id)
+    vm = Downvote(post = post, downvote = 1)
+    vm.save()
+    return redirect(url_for('main.posts'))
