73ee224be553f6801aa127d90b4c965838e2a2cf
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-29 01:14:09 +0300

    Deployment to heroku.

--- config.py
+++ config.py
@@ -23,7 +23,8 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-   
+    
+    
 class TestConfig(Config):
     '''
     Testing configuration child class
